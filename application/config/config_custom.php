fc4128c7cb68e1d24cd3a194109889bc5918c1df
Rain <serip88@gmail.com>
2017-05-26 10:51:44 +0700

    + add htaccess, controller admin

--- application/config/config_custom.php
+++ application/config/config_custom.php
@@ -4,3 +4,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['base_url'] = strpos($_SERVER['HTTP_HOST'],"www")===0?'http://'.$_SERVER['HTTP_HOST'].'/':'http://www.'.$_SERVER['HTTP_HOST'].'/';
 $config['site_name'] = 'Ungdungnho';
 $config['site_version'] = '1.0';
+$config['app_path'] = 'app_change';
+$config['app_admin_path'] = 'app_change_ad';
