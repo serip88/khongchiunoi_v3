85f2c685fe95c0d804b6780d11cb3407de32b2f7
Rain <serip88@gmail.com>
2017-06-07 22:12:19 +0700

    handle get api user_ss for 2 type

--- application/controllers/app_change/api/User.php
+++ application/controllers/app_change/api/User.php
@@ -94,7 +94,12 @@ class User extends Base_controller {
     public function user_ss_get()
     {
         $status = false;
-        $data_user = $this->user_lib->get_user_session();
+        $_user_type = isset($param['t']) && $param['t'] ? $param['t'] : '';
+        if($_user_type=='a'){
+            $data_user = $this->user_lib->get_user_session();
+        }else{
+            $data_user = $this->user_lib->get_member_session();
+        }
         if($data_user){
             $status = true;
         }
