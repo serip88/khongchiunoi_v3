d3082d75b1c44d277ab7eb0f355219dda67006aa
Rain <serip88@gmail.com>
2018-09-01 16:17:07 +0700

    update

--- application/views/app_pay/index.php
+++ application/views/app_pay/index.php
@@ -6,7 +6,7 @@
         
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Estore Multipurpose eCommerce Template</title>
+        <title>Apple super cheap</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
